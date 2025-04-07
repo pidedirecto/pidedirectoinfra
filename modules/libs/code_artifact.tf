304a8ecc7c23d82635b03f3d9c11517fa858abee
Jorge L. <jorge.alfmur@gmail.com>
2025-04-07 16:12:57 -0500

    refactor: lib module

--- modules/libs/code_artifact.tf
+++ modules/libs/code_artifact.tf
@@ -5,11 +5,21 @@ resource "aws_codeartifact_domain" "pidedirecto_domain" {
 resource "aws_codeartifact_repository" "pidedirecto_repo" {
   repository = var.repository_name
   domain     = aws_codeartifact_domain.pidedirecto_domain.domain
+
+  lifecycle {
+    ignore_changes = [ external_connections ]
+  }
 }
 
 resource "null_resource" "associate_npm_connection" {
   provisioner "local-exec" {
     command = "aws codeartifact associate-external-connection --domain ${aws_codeartifact_domain.pidedirecto_domain.domain} --repository ${aws_codeartifact_repository.pidedirecto_repo.repository} --external-connection public:npmjs --profile ${var.aws_profile}"
+  }
+
+  triggers = {
+    domain_name         = var.domain_name
+    repository_name     = var.repository_name
+    external_connection = "public:npmjs"
   }
 
   depends_on = [aws_codeartifact_repository.pidedirecto_repo]
