72ee014a1165c1a5a33b7ffded88fa56056954eb
PipoLucido <monntedeoca@hotmail.com>
2025-06-06 13:07:07 -0300

    fix alb only one target group is enough

--- modules/retail/alb.tf
+++ modules/retail/alb.tf
@@ -1,7 +1,6 @@
 resource "aws_lb_target_group" "retail" {
-  for_each = { for image in var.images : image.name => image }
 
-  name     = "tg-${each.key}"
+  name     = "tg-retail"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
@@ -18,19 +17,19 @@ resource "aws_lb_target_group" "retail" {
   }
  }
 
-resource "aws_lb_listener_rule" "retail" {
-  for_each = { for image in var.images : image.name => image }
-
-  listener_arn = aws_lb_listener.http.arn
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.retail[each.key].arn
-  }
-
-  condition {
-    path_pattern {
-      values = [each.value.path]
-    }
-  }
-}
+#resource "aws_lb_listener_rule" "retail" {
+#  for_each = { for image in var.images : image.name => image }
+#
+#  listener_arn = aws_lb_listener.http.arn
+#
+#  action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.retail[each.key].arn
+#  }
+#
+#  condition {
+#    path_pattern {
+#      values = [each.value.path]
+#    }
+#  }
+#}
--- modules/retail/ecs_services.tf
+++ modules/retail/ecs_services.tf
@@ -1,9 +1,9 @@
 resource "aws_ecs_service" "retail" {
   //for_each = { for image in var.images : image.name => image } // this is looping on all images, for now its comment to only deploy only service (one deploy, one project)
 
-  name            = "retail-php" //each.value.name
+  name            = "retail-billing-php" //each.value.name
   cluster         = aws_ecs_cluster.retail.id
-  task_definition = "ambit-retail-php-task:1" //each.value.task_definition
+  task_definition = "ambit-retail-php-task:4" //each.value.task_definition
   launch_type     = "FARGATE"
   desired_count   = 1
 
@@ -15,8 +15,8 @@ resource "aws_ecs_service" "retail" {
 
   load_balancer {
     target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:264272770386:targetgroup/tg-retail/fbc0cc131eb37e0e"
-    container_name   = "retail-php"  # Must match container name in task definition
+    container_name   = "retail-billing-php"  # Must match container name in task definition
     container_port   = 80            # Must match port container is exposing
   }
 
-}
+}//duplicate this step instead of removing the old one check with jorge and use only one command to deploy all with depends on
