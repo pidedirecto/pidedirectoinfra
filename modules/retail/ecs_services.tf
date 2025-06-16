b4d2f72b832a09884283ee913287a3e76f1a0a91
PipoLucido <monntedeoca@hotmail.com>
2025-06-16 16:24:56 -0300

    fix iam ecs_task_execution_role

--- modules/retail/ecs_services.tf
+++ modules/retail/ecs_services.tf
@@ -70,7 +70,7 @@ resource "aws_ecs_service" "retail" {
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+  name = "retailEcsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
