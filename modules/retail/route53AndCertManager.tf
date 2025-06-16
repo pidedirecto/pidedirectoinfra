259de9808c0a88c02e17e014166f3bddd1c5f2d2
PipoLucido <monntedeoca@hotmail.com>
2025-06-16 16:21:56 -0300

    fix domains on cert manager

--- modules/retail/route53AndCertManager.tf
+++ modules/retail/route53AndCertManager.tf
@@ -5,7 +5,7 @@ resource "aws_route53_zone" "main" {
 resource "aws_acm_certificate" "wildcard" {
   domain_name               = "ambitretail.com"
   validation_method         = "DNS"
-  subject_alternative_names = ["*.ambitretail.com"]
+  subject_alternative_names = ["api.ambitretail.com"]
 
   lifecycle {
     create_before_destroy = true
