751e1f38ab37762bd40cdb99ed12a6e9155c5f8c
PipoLucido <monntedeoca@hotmail.com>
2025-06-25 16:30:41 -0300

    add 443 to sg

--- modules/retail/security_groups.tf
+++ modules/retail/security_groups.tf
@@ -40,6 +40,14 @@ resource "aws_security_group" "retail_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
